0eec258bc311a4bb2c7dd12337e51c736cebb864
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-04-18 01:59:21 -0400

    fix: rules to show current component

--- src/components/MultiStepForm/index.tsx
+++ src/components/MultiStepForm/index.tsx
@@ -1,28 +1,48 @@
-import { useState } from 'react';
-import { IdentificationPet } from '../../components/IdentificationPet';
-import { InfosAddPet } from '../../components/InfosAddPet';
-import { AddPhotoPed } from '../../components/AddPhotoPet';
 import { ButtonPrevNext, MainContainer } from './styles';
-import { useInput } from '../../hooks/useInput';
-export const MultiStepForm = () => {
-    const { dataPet } = useInput();
-    const steps = [<IdentificationPet />, <InfosAddPet />, <AddPhotoPed />];
+import UserForm from '../UserForm/userForm';
+import { useForm } from '../../hooks/useForm';
+import PetPrimaryForm from '../PetPrimaryForm/petPrimaryForm';
+import PetSecondaryForm from '../PetSecondaryForm/petSecondaryForm';
+import { IPet } from '../../interfaces/IPet';
+import { ChangeEvent, useState } from 'react';
 
-    const [step, setStep] = useState(0);
-    const handleSubmit = () => {
-        console.log(dataPet);
+const formBuilder: IPet = {
+    owner: '',
+    mail: '',
+    size: '',
+    weight: '',
+    sex: '',
+    color: '',
+    specie: '',
+};
+
+export const MultiStepForm = () => {
+    const [data, setData] = useState(formBuilder);
+    const changeValueInput = (key: string, value: string) => {
+        console.log(key, value);
+
+        setData((prev) => {
+            return { ...prev, [key]: value };
+        });
     };
+    const stepsComponents = [
+        <UserForm data={data} externalFunc={changeValueInput} />,
+        <PetPrimaryForm />,
+        <PetSecondaryForm />,
+    ];
+    const { step, changeStep } = useForm(stepsComponents);
+
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         if (step >= 3 || step < 0) return;
-        if (e.currentTarget.innerText === 'Avançar') setStep(step + 1);
-        if (e.currentTarget.innerText != 'Avançar') setStep(step - 1);
+        if (e.currentTarget.innerText === 'Avançar') changeStep(true);
+        if (e.currentTarget.innerText != 'Avançar') changeStep(false);
     };
 
     return (
         <MainContainer>
-            <h1></h1>
+            <h1>Olá! vamos cadastrar algumas informações do seu Pet </h1>
             <form>
-                {steps[step]}
+                {stepsComponents[step]}
                 {step != 0 && (
                     <ButtonPrevNext
                         borderColor=' #f00'
@@ -35,8 +55,11 @@ export const MultiStepForm = () => {
                 {step >= 2 && (
                     <ButtonPrevNext
                         borderColor=' #10454f'
-                        type='button'
-                        onClick={handleSubmit}
+                        type='submit'
+                        onClick={(e) => {
+                            e.preventDefault();
+                            console.log(data);
+                        }}
                     >
                         Finalizar
                     </ButtonPrevNext>
