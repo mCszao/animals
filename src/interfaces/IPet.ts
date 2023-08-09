92ee490dccd9dd5e5dbab6b9f47ecb5578ff02d6
kuohakuu <110683088+mCszao@users.noreply.github.com>
2023-08-09 01:55:01 -0400

    refact: dto pet separate from owner informations

--- src/interfaces/IPet.ts
+++ src/interfaces/IPet.ts
@@ -1,9 +1,9 @@
 export interface IPet {
-    owner: string;
-    mail: string;
-    size: string;
-    weight: string;
-    sex: string;
-    color: string;
-    specie: string;
+    petName: string;
+    size?: string;
+    weight?: string;
+    sex?: string;
+    color?: string;
+    specie?: string;
+    about?: string;
 }
